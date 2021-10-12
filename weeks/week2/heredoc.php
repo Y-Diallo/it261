0141f7c064007eaec8fc8248eaf4364fa2afabe7
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-10-12 02:06:20 -0700

    Update heredoc.php

--- weeks/week2/heredoc.php
+++ weeks/week2/heredoc.php
@@ -1,7 +1,18 @@
 <?php 
-    echo "hello world"
+    $book = 'Handmaid\'s Tale';
+    $author = 'Margaret Atwood';
+    $character = 'June';
+    $actor = 'Elizabeth Moss';
 
-    $content = 'My favorite book is Handmaid\'s Tale, written by Margaret Atwood, and is presently a miniseries on HULU. Hulu\'s viewing audience is extremely excited about the miniseries and looks forward to the 5th season of the award-winning "Handmaid\'s Tale!"
+    $content = <<<HAND
+    My favorite book is $book, written by $author, and is presently a miniseries on 
+    HULU. Hulu's viewing audience is extremely excited about the miniseries and 
+    looks forward to the 5th season of the award-winning "Handmaid's Tale!"
 
-    Elizabeth Moss\'s rendition as June is right on! Once this content is corrected with "escapes" and nesting my variables inside \'.    .\'    it will display properly; my content is displayed in red (not orange), and my variables are displayed in black!!!';
+    $actor's rendition as $character is right on! Once this content is corrected, it 
+    will display properly; my content is displayed in orange, and my variables are 
+    displayed in black!!! 
+    HAND;
+    
+    echo $content;
 ?>
