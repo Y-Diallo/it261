50504c7777a38a7ca8aedaf6a2a13031632d9af5
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-10-11 19:09:56 -0700

    added to portal page

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<!doctype html>
+<html>
 <?php 
     echo "hello world chung"
 ?>
+<header>
+    <h1>My Main Assignments!</h1>
+</header>
+<main>
+    <h2>About Me!</h2>
+    <p>paragraph about me</p>
+    <h2>My Classwork Exercises</h2>
+    <h3>Week 2</h3>
+    <h2>Mamp screenshots</h2>
+</main>
+<aside></aside>
+<footer></footer>
+</html>
