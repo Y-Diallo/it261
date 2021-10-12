edc3267eb06a099a5641b8f0fb3badb4ddba57fd
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-10-12 01:58:36 -0700

    Update heredoc.php

--- weeks/week2/heredoc.php
+++ weeks/week2/heredoc.php
@@ -1,7 +1,7 @@
 <?php 
     echo "hello world"
 
-    $content = 'My favorite book is Handmaid\'s Tale, written by Margaret Atwood, and is presently a miniseries on HULU. Hulu\'s viewing audience is extremely excited about the miniseries and looks forward to the 5th season of the award-winning \"Handmaid\'s Tale!\"
+    $content = 'My favorite book is Handmaid\'s Tale, written by Margaret Atwood, and is presently a miniseries on HULU. Hulu\'s viewing audience is extremely excited about the miniseries and looks forward to the 5th season of the award-winning "Handmaid\'s Tale!"
 
-    Elizabeth Moss\'s rendition as June is right on! Once this content is corrected with \"escapes\" and nesting my variables inside \'.    .\'    it will display properly; my content is displayed in red (not orange), and my variables are displayed in black!!!';
+    Elizabeth Moss\'s rendition as June is right on! Once this content is corrected with "escapes" and nesting my variables inside \'.    .\'    it will display properly; my content is displayed in red (not orange), and my variables are displayed in black!!!';
 ?>
