ff4ba7863778efc1affe4727593ac206387c28ce
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-10-11 21:59:32 -0700

    added to base page

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html>
+<html class="page">
 <?php 
     echo "hello world chung"
 ?>
@@ -8,11 +8,19 @@
 </header>
 <main>
     <h2>About Me!</h2>
-    <p>paragraph about me</p>
+    <p>I'm Youssoupha Diallo, a running start student at Lincoln High School!<br> I play games and upload them as videos to my youtube channel Yman!</p>
+    <h3><a href=https://www.youtube.com/channel/UCPnurA7JJ71RDnHQze5dsZw>MY CHANNEL :DD</a></h3>
+    <img src="/it261/profile.png" alt="whomegalul">
     <h2>My Classwork Exercises</h2>
     <h3>Week 2</h3>
     <p><a href="/it261/weeks/week2/var.php">var.php</a></p>
+    <p><a href="/it261/weeks/week2/var2.php">var2.php</a></p>
+    <p><a href="/it261/weeks/week2/currency-logic.php">currency-logic.php</a></p>
+    <p><a href="/it261/weeks/week2/currency.php">currency.php</a></p>
+    <p><a href="/it261/weeks/week2/heredoc.php">heredoc.php</a></p>
     <h2>Mamp screenshots</h2>
+    <img src="/it261/welcomeMAMP.png" alt="welcome to mamp screenshot">
+    <img src="/it261/errorMAMP.png" alt="mamp error screenshot">
 </main>
 <aside></aside>
 <footer></footer>
