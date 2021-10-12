5a7e4650b57ba91909c7216205c0accbb9127092
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-10-11 22:13:42 -0700

    naming

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <!doctype html>
 <html class="page">
-    <header>
-        <link rel="stylesheet" href="styles.css">
-        <h1>My Main Assignments!</h1>
-    </header>
-    <main>
-        <h2>About Me!</h2>
-        <p>I'm Youssoupha Diallo, a running start student at Lincoln High School!<br> I play games and upload them as videos to my youtube channel Yman!</p>
-        <h3><a href=https://www.youtube.com/channel/UCPnurA7JJ71RDnHQze5dsZw target="_blank">MY CHANNEL :DD</a></h3>
-        <img src="/it261/profile.png" alt="whomegalul">
-        <h2>My Classwork Exercises</h2>
-        <h3>Week 2</h3>
-        <p><a href="/it261/weeks/week2/var.php">var.php</a></p>
-        <p><a href="/it261/weeks/week2/var2.php">var2.php</a></p>
-        <p><a href="/it261/weeks/week2/currency-logic.php">currency-logic.php</a></p>
-        <p><a href="/it261/weeks/week2/currency.php">currency.php</a></p>
-        <p><a href="/it261/weeks/week2/heredoc.php">heredoc.php</a></p>
-        <h2>Mamp screenshots</h2>
-        <img src="/it261/welcomeMAMP.png" alt="welcome to mamp screenshot">
-        <img src="/it261/errorMAMP.png" alt="mamp error screenshot">
-    </main>
-    <aside></aside>
-    <footer></footer>
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <h1>My Main Assignments!</h1>
+</head>
+<body>
+    <h2>About Me!</h2>
+    <p>I'm Youssoupha Diallo, a running start student at Lincoln High School!<br> I play games and upload them as videos to my youtube channel Yman!</p>
+    <h3><a href=https://www.youtube.com/channel/UCPnurA7JJ71RDnHQze5dsZw target="_blank">MY CHANNEL :DD</a></h3>
+    <img src="/it261/profile.png" alt="whomegalul">
+    <h2>My Classwork Exercises</h2>
+    <h3>Week 2</h3>
+    <p><a href="/it261/weeks/week2/var.php">var.php</a></p>
+    <p><a href="/it261/weeks/week2/var2.php">var2.php</a></p>
+    <p><a href="/it261/weeks/week2/currency-logic.php">currency-logic.php</a></p>
+    <p><a href="/it261/weeks/week2/currency.php">currency.php</a></p>
+    <p><a href="/it261/weeks/week2/heredoc.php">heredoc.php</a></p>
+    <h2>Mamp screenshots</h2>
+    <img src="/it261/welcomeMAMP.png" alt="welcome to mamp screenshot">
+    <img src="/it261/errorMAMP.png" alt="mamp error screenshot">
+</body>
+<aside></aside>
+<foot></foot>
 </html>
