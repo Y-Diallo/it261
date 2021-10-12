d651c4bbcaabebd442a3e9355a1ba0e5398e8f15
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-10-11 23:48:17 -0700

    positioning

--- index.php
+++ index.php
@@ -9,23 +9,28 @@
 <header>
     <h1>My Main Assignments!</h1>
 </header>
-<main>
+<main class="leftSideBar">
     <h2>About Me!</h2>
-    <p>I'm Youssoupha Diallo, a running start student at Lincoln High School!<br> I play games and upload them as videos to my youtube channel Yman!</p>
+    <p>I'm Youssoupha Diallo, a running start student at Lincoln High School!<br>
+    My hobbies include gaming and video editing <br>
+     I play games and upload them as videos to my youtube channel Yman!<br>
+     It takes quite a bit of time and I'm getting back into the swing of it.
+    </p>
     <h3><a href=https://www.youtube.com/channel/UCPnurA7JJ71RDnHQze5dsZw target="_blank">MY CHANNEL :DD</a></h3>
     <img src="/it261/images/profile.png" alt="whomegalul">
-    <h2>My Classwork Exercises</h2>
+    <h2>Mamp screenshots</h2>
+    <img src="/it261/images/welcomeMAMP.png" alt="welcome to mamp screenshot">
+
+
+</main>
+<aside class="rightSide">
+<h2>My Classwork Exercises</h2>
     <h3>Week 2</h3>
     <p><a href="/it261/weeks/week2/var.php">var.php</a></p>
     <p><a href="/it261/weeks/week2/var2.php">var2.php</a></p>
     <p><a href="/it261/weeks/week2/currency-logic.php">currency-logic.php</a></p>
     <p><a href="/it261/weeks/week2/currency.php">currency.php</a></p>
     <p><a href="/it261/weeks/week2/heredoc.php">heredoc.php</a></p>
-
-</main>
-<aside>
-    <h2>Mamp screenshots</h2>
-    <img src="/it261/images/welcomeMAMP.png" alt="welcome to mamp screenshot">
 </aside>
 <footer></footer>
 </html>
