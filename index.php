568767731f06b4b23ca0039691edd7b63e4b3b92
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-10-18 01:24:03 -0700

    changed organization

--- index.php
+++ index.php
@@ -1,41 +1,57 @@
 <!doctype html>
-<html lang="en" class="page">
-<head>
-    <link rel="stylesheet" href="css/styles.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0" >
-    <title>Youssoupha Diallo</title>
-</head>
-<header>
-    <h1>My Main Assignments!</h1>
-</header>
-<main class="leftSideBar">
-    <h2>About Me!</h2>
-    <p>I'm Youssoupha Diallo, a running start student at Lincoln High School!<br>
-    My hobbies include gaming and video editing <br>
-     I play games and upload them as videos to my youtube channel Yman!<br>
-     It takes quite a bit of time and I'm getting back into the swing of it.
-    </p>
-    <h3><a href=https://www.youtube.com/channel/UCPnurA7JJ71RDnHQze5dsZw target="_blank">MY CHANNEL</a></h3>
-    <img src="/it261/images/profile.png" alt="whomegalul">
-</main>
-<aside class="rightSide">
-    <h2>My Classwork Exercises</h2>
-    <h3>Week 2</h3>
-    <p><a href="/it261/weeks/week2/var.php">var.php</a></p>
-    <p><a href="/it261/weeks/week2/var2.php">var2.php</a></p>
-    <p><a href="/it261/weeks/week2/currency-logic.php">currency-logic.php</a></p>
-    <p><a href="/it261/weeks/week2/currency.php">currency.php</a></p>
-    <p><a href="/it261/weeks/week2/heredoc.php">heredoc.php</a></p>
-    <h3>Week 3</h3>
-    <p><a href="/it261/weeks/week3/var.php">var.php</a></p>
-    <h3>Validation!</h3>
-    <li><a href="http://validator.w3.org/check?uri=referer">HTML</a></li>
-    <li><a href="http://jigsaw.w3.org/css-validator/check?uri=referer">CSS</a></li>
-</aside>
-<footer>    
-    <h2>Mamp screenshots:</h2>
-    <img src="/it261/images/welcomeMAMP.png" alt="welcome to mamp screenshot">
-    <img src="/it261/images/testMAMP.png" alt="mamp error sceenshot">
-</footer>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="css/styles.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" >
+        <title>Youssoupha Diallo</title>
+    </head>
+    <header>
+        <h1>My Main Assignments!</h1>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Switch(3)</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div id="wrapper">
+        <main class="leftSideBar">
+            <h2>About Me!</h2>
+            <p>I'm Youssoupha Diallo, a running start student at Lincoln High School!<br>
+            My hobbies include gaming and video editing <br>
+            I play games and upload them as videos to my youtube channel Yman!<br>
+            It takes quite a bit of time and I'm getting back into the swing of it.
+            </p>
+            <h3><a href=https://www.youtube.com/channel/UCPnurA7JJ71RDnHQze5dsZw target="_blank">MY CHANNEL</a></h3>
+            <img src="/it261/images/profile.png" alt="whomegalul">
+        </main>
+        <aside class="rightSide">
+            <h2>My Classwork Exercises</h2>
+            <h3>Week 2</h3>
+            <p><a href="/it261/weeks/week2/var.php">var.php</a></p>
+            <p><a href="/it261/weeks/week2/var2.php">var2.php</a></p>
+            <p><a href="/it261/weeks/week2/currency-logic.php">currency-logic.php</a></p>
+            <p><a href="/it261/weeks/week2/currency.php">currency.php</a></p>
+            <p><a href="/it261/weeks/week2/heredoc.php">heredoc.php</a></p>
+            <h3>Week 3</h3>
+            <p><a href="/it261/weeks/week3/var.php">var.php</a></p>
+            <h3>Validation!</h3>
+            <li><a href="http://validator.w3.org/check?uri=referer">HTML</a></li>
+            <li><a href="http://jigsaw.w3.org/css-validator/check?uri=referer">CSS</a></li>
+        </aside>
+        
+        <footer>
+        <h3>Validation!</h3>
+            <ul>
+                <li><a href="http://validator.w3.org/check?uri=referer">HTML</a></li>
+                <li><a href="http://jigsaw.w3.org/css-validator/check?uri=referer">CSS</a></li> 
+            </ul>
+        </footer>
+        <div>
+            <h2>Mamp screenshots:</h2>
+            <img src="/it261/images/welcomeMAMP.png" alt="welcome to mamp screenshot">
+            <img src="/it261/images/testMAMP.png" alt="mamp error sceenshot">
+        </div>
+    </div>
 </html>
