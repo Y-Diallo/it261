c6679c585119aee5893059315a92b491fa6da478
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-10-18 05:55:52 -0700

    Update daily.php

--- website/daily.php
+++ website/daily.php
@@ -65,13 +65,13 @@
     <h2>Check out our Daily Specials</h2>
 
     <ul>
-    <li><a href="switch.php?today=Sunday">Sunday</a></li>
-    <li><a href="switch.php?today=Monday">Monday</a></li>
-    <li><a href="switch.php?today=Tuesday">Tuesday</a></li>
-    <li><a href="switch.php?today=Wednesday">Wednesday</a></li>
-    <li><a href="switch.php?today=Thursday">Thursday</a></li>
-    <li><a href="switch.php?today=Friday">Friday</a></li>
-    <li><a href="switch.php?today=Saturday">Saturday</a></li>
+    <li><a href="daily.php?today=Sunday">Sunday</a></li>
+    <li><a href="daily.php?today=Monday">Monday</a></li>
+    <li><a href="daily.php?today=Tuesday">Tuesday</a></li>
+    <li><a href="daily.php?today=Wednesday">Wednesday</a></li>
+    <li><a href="daily.php?today=Thursday">Thursday</a></li>
+    <li><a href="daily.php?today=Friday">Friday</a></li>
+    <li><a href="daily.php?today=Saturday">Saturday</a></li>
     </ul>
     </div>
     </main>
