eee5a7f649cf44529813db91e13dc06cd1e0c780
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-10-24 21:06:36 -0700

    Update form-arithmetic.php

---
+++ weeks/week4/form-arithmetic.php
@@ -0,0 +1,101 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Arithmetic Form</title>
+<style>
+    form {
+        width: 400px;
+        margin: 0 auto;
+    }
+
+    fieldset {
+        border: 1px solid red;
+        padding: 10px;
+    }
+
+    label, textarea {
+        display: block;
+        margin-bottom: 5px;
+    }
+
+    input, textarea {
+        margin-bottom: 10px;
+    }
+
+    input[type=text]{
+        width: 90%;
+    }
+    
+    input[type=number]{
+        width: 60%;
+    }
+    h3 {
+        text-align: center;
+        color: red;
+    }
+
+    .box {
+        width: 400px;
+        margin:20px auto;
+        padding: 15px;
+        background: lightgray;
+    }
+
+</style>
+</head>
+
+<body>
+    <form action="" method="POST">
+        <fieldset>
+            <label for="fname">First Name</label>
+            <input type="text" name="fname">
+
+            <label for="coffees">How many coffees?</label>
+            <input type="number" name="coffees">
+
+            <label for="lattes">How many lattes?</label>
+            <input type="number" name="lattes">
+            
+            <label for="caps">How many cappucinos?</label>
+            <input type="number" name="caps">
+
+            <input type="submit" value="Send">
+
+        </fieldset>
+    </form>
+    <?php 
+    if(isset($_POST['fname'],$_POST['coffees'], $_POST['lattes'], $_POST['caps'])){
+        $fname = $_POST['fname'];
+        $coffees = $_POST['coffees'];
+        $lattes = $_POST['lattes'];
+        $caps = $_POST['caps'];
+        
+        $total = $coffees + $lattes + $caps;
+        if(empty($fname && $coffees && $lattes && $caps)){
+            //failcase
+            echo '<h3>Please fill out the fields!</h3>';
+        } else {
+            //success
+            // echo ''.$fname.''.$coffees.''.$lattes.''.$caps.'';
+
+            echo '
+            <div class="box">
+                <h2>Hello '.$fname.'</h2>
+                <p>You have ordered the following:</p>
+                <ul>
+                    <li>'.$coffees.' Coffees</li>
+                    <li>'.$lattes.' Lattes</li>
+                    <li>'.$caps.' Cappucinos</li>
+                </ul>
+                <p>Totalling '.$total.' beverages!</p>
+            </div>
+            ';
+        }
+    }
+
+
+    ?>
+</body>
+</html>
