71f46d3da36a88c15984865f5a512cee1b83b26a
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-11-02 03:32:18 -0700

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -91,8 +91,11 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $speed = 65;//65 mph
         $gallons = $miles/$MPG;
         $total = $gallons*$price;
+        $friendly_total = @number_format($total, 2);
         $totalHours = $miles/$speed;
+        $friendly_hours = ceil($hours);
         $days = $totalHours/$hours;
+        $friendly_days = ceil($days);
         
         
         echo '
@@ -100,8 +103,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
             <h2>Calculator Results</h2>
             <p> '.$name.', you will be driving <b>'.$miles.' miles</b><br>
             Your vehicle has an efficieny rating of <b>'.$MPG.' miles per gallon</b>
-            Your total cost for gas will be <b>$'.$total.' dollars</b>
-            You will be driving a total of <b>'.$totalHours.' hours equating to '.$days.' days</b>
+            Your total cost for gas will be <b>$'.$friendly_total.' dollars</b>
+            You will be driving a total of <b>'.$friendly_hours.' hours equating to '.$friendly_days.' days</b>
             </p>
             
         </div>';
