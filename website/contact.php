4da44a378074a10da81fb885e3cf657d81ac9d79
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-11-16 04:39:34 -0800

    css

--- website/contact.php
+++ website/contact.php
@@ -5,7 +5,7 @@
 <div id="wrapper">
     <main>
     <h1><?php echo $headline;?></h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et</p>
     <h2>Please fill out our form!</h2>
     <?php 
     include('includes/form.php');
