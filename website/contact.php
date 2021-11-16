698f178c1d34eb645211645302d9e30ee1d90a1b
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-11-16 04:45:11 -0800

    css

--- website/contact.php
+++ website/contact.php
@@ -3,14 +3,14 @@
     include('includes/header.php');
 ?>
 <div id="wrapper">
-    <div class="formMain">
+    <main>
     <h1><?php echo $headline;?></h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et</p>
     <h2>Please fill out our form!</h2>
     <?php 
     include('includes/form.php');
     ?>
-    </div>
+    </main>
 
     <aside>
     <h3>Our Aside for our Form</h3>
