d0ca280dfbf437a0da7451307ef921aa7a886df1
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-11-30 11:54:07 -0800

    w9

---
+++ weeks/week9/credentials.php
@@ -0,0 +1,5 @@
+<?php
+define('DB_NAME', 'winnepov_fall2021');
+define('DB_USER', 'winnepov_falluse');
+define('DB_PASSWORD', 'fall2021_user');
+define('DB_HOST', 'localhost');
---
+++ weeks/week9/people.php
@@ -0,0 +1,43 @@
+<?php 
+
+include('config.php');
+include('includes/header.php');
+?>
+<main>
+<h1>Welcome to my People page Class Exercise</h1>
+
+<?php
+$sql = 'SELECT * FROM people';
+$iConn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die(myError(__FILE__,__LINE__,mysqli_connect_error()));
+
+$result = mysqli_query($iConn, $sql) or die(myError(__FILE__,__LINE__,mysqli_error($iConn)));
+
+
+if(mysqli_num_rows($result) > 0){
+    while($row = mysqli_fetch_assoc($result)){
+        echo '<h3> For more information about '.$row['first_name'].', please click <a href="people-view.php?id='.$row['people_id'].'">here!</a></h3>';
+        echo '<ul>';
+        echo '<li>'.$row['first_name'].'</li>';
+        echo '<li>'.$row['last_name'].'</li>';
+        echo '<li>'.$row['email'].'</li>';
+        echo '</ul>';
+        echo '<hr>';
+    }
+} else {
+    echo 'Houston, we have a problem';
+}
+
+mysqli_free_result($result);
+mysqli_close($iConn);
+?>
+</main>
+<aside>
+
+
+
+</aside>
+</div>
+
+<?php
+include('includes/footer.php');
+?>
