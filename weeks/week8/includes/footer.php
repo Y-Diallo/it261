7763dc29adf6edb78274af205b323c875c12699f
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-11-30 13:22:01 -0800

    Update footer.php

--- weeks/week8/includes/footer.php
+++ weeks/week8/includes/footer.php
@@ -1,19 +1,25 @@
 <footer>
-            <ul>
-                <li>Copyright &copy; <?php
-                 $dateCurrent = idate('Y');
-                 $dateCreated = 2021;
-                 if($dateCurrent == $dateCreated) {
-                    echo $dateCreated;
-                 } else {
-                    echo ''.$dateCreated.' - '.$dateCurrent.'    ';
-                 }
-                 ?></li>
-                <li>All Rights Reserved</li>
-                <li><a href="../../index.php"></a>Web Design by Youssoupha</li>
-                <li><a href="http://validator.w3.org/check?uri=referer">HTML Validation</a></li>
-                <li><a href="http://jigsaw.w3.org/css-validator/check?uri=referer">CSS Validation</a></li> 
-            </ul>
+                <ul>
+                    <li>Copyright &copy; <?php
+                    $dateCurrent = idate('Y');
+                    $dateCreated = 2021;
+                    if($dateCurrent == $dateCreated) {
+                        echo $dateCreated;
+                    } else {
+                        echo ''.$dateCreated.' - '.$dateCurrent.'    ';
+                    }
+                    ?></li>
+                    <<li>All Rights Reserved </li>
+                    <li><a href="">Terms of use</a></li>
+                    <li><a href="../index.php">Portal page</a></li>
+                    <li> <a id="html-checker" href="#">Check HTML</a></li>
+                    <li><a id="css-checker" href="#">Check CSS</a></li>
+
+                    <script>
+                        document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+                        document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+                    </script>
+                </ul>
             </footer>
         </div>
     </body>
