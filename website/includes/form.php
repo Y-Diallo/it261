b81e24b0fbef98de850bab29321881e05ca24a81
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-11-30 13:14:11 -0800

    label

--- website/includes/form.php
+++ website/includes/form.php
@@ -2,20 +2,20 @@
 <?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>
 " method="POST">
     <fieldset>
-        First Name
+        <span class="label">First Name</span>
         <input type="text" name="first_name" value="<?php if(isset($_POST['first_name'])){ echo htmlspecialchars($_POST['first_name']);} ?>">
         <?php echo $first_name_Err?>
-        Last Name
+        <span class="label">Last Name</span>
         <input type="text" name="last_name" value="<?php if(isset($_POST['last_name'])){ echo htmlspecialchars($_POST['last_name']);} ?>">
         <?php echo $last_name_Err?>
-        Email
+        <span class="label">Email</span>
         <input type="text" name="email" value="<?php if(isset($_POST['email'])){ echo htmlspecialchars($_POST['email']);} ?>">
         <?php echo $email_Err?>
-        Phone Number
+        <span class="label">Phone Number</span>
         <input type="text" name="phone" placeholder="xxx-xxx-xxxx" value="<?php if(isset($_POST['phone'])){ echo htmlspecialchars($_POST['phone']);} ?>">
         <?php echo $phone_Err?>
 
-        Gender
+        <span class="label">Gender</span>
         <ul>
             <li><input type="radio" id="male" name="gender" value="male"
             <?php if(isset($_POST['gender']) && $_POST['gender'] == 'male'){ echo 'checked="checked"';}?>><label for="male">Male</label></li>
@@ -26,7 +26,7 @@
         </ul>
         <?php echo $gender_Err?>
 
-        Favorite Tasks
+        <span class="label">Favorite Tasks</span>
         <ul>
             <li><input type="checkbox" id="admin" name="tasks[]" value="admin"
             <?php if(isset($_POST['tasks']) && in_array('admin',$tasks)){ echo 'checked="checked"';}?>><label for="admin">Admin</label></li>
@@ -41,7 +41,7 @@
         </ul>
         <?php echo $tasks_Err?>
 
-        Among Us Color
+        <span class="label">Among Us Color</span>
         <select name="colors">
             <option value="" NULL 
             <?php if(isset($_POST['colors']) && $_POST['colors'] == NULL){ echo 'selected="unselected"';}?>>Select one!</option>
@@ -66,11 +66,11 @@
         </select>
         <?php echo $colors_Err?>
 
-        Comments
+        <span class="label">Comments</span>
         <textarea name="comments"><?php if(isset($_POST['comments'])){ echo htmlspecialchars($_POST['comments']);} ?></textarea>
         <?php echo $comments_Err?>
 
-        Privacy
+        <span class="label">Privacy</span>
         <ul>
             <li><input type="radio" id="agreed" name="privacy" value="agreed"
             <?php if(isset($_POST['privacy']) && $_POST['privacy'] == 'agreed'){ echo 'checked="checked"';}?>
