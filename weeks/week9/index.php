c1084bd449db21da1575ffc5594dd02b228023c4
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-12-07 05:00:24 -0800

    Update index.php

--- weeks/week9/index.php
+++ weeks/week9/index.php
@@ -2,43 +2,30 @@
 session_start();
 include('config.php');
 
-$sql = 'SELECT * FROM people';
-$iConn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die(myError(__FILE__,__LINE__,mysqli_connect_error()));
-
-$result = mysqli_query($iConn, $sql) or die(myError(__FILE__,__LINE__,mysqli_error($iConn)));
-
-
-?>
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>testing week8 sql</title>
-<link href="css/styles.css" type="text/css" rel="stylesheet">
-</head>
-
-<body>
-    <table>
-        <?php 
-        
-if(mysqli_num_rows($result) > 0){
-while($row = mysqli_fetch_assoc($result)){
-echo
-'<tr>
-<td><img src="images/'.(substr($row['email'],0, -9)).'.jpg" alt="'.$row['first_name'].' '.$row['last_name'].'"></td>
-<td>'.$row['first_name'].' '.$row['last_name'].'</td>
-<td>'.$row['occupation'].'</td>
-<td><img src="images/'.(substr($row['email'],0, -8)).'.jpg" alt="'.$row['first_name'].' '.$row['last_name'].'"></td>
-</tr>';
+if(!isset($_SESSION['username'])){
+    $_SESSION['msg'] = 'You must login first!';
+    header('Location: login.php');
 }
-} else {
-
+if(isset($_GET['logout'])){
+    session_destroy();
+    unset($_SESSION['username']);
+    header('Location: login.php');
 }
+include('includes/header.php');
+if(isset($_SESSION['success'])) :?>
+<div class="success">
+    <h3><?php echo $_SESSION['success'];
+unset($_SESSION['success']); ?></h3>
+</div>
+<?php endif;?>
+<?php
+if(isset($_SESSION['username'])) :?>
+<div class="welcome-logout">
+    <h3>Hello <?php echo $_SESSION['username']; ?>!</h3>
+    <p><a href="index.php?logout='1'">Log out</a></p>
+</div>
+<?php endif;?>
 
-mysqli_free_result($result);
-mysqli_close($iConn);
-
-?>
-    </table>
-</body>
-</html>
+<main>
+</main>
+</div>
