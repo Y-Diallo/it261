6efac865d86e8e94ba748d6b362b6704237a06f1
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-12-07 13:33:04 -0800

    edit headers

--- weeks/week9/includes/header.php
+++ weeks/week9/includes/header.php
@@ -1,11 +1,10 @@
 <!doctype html>
 <html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Week 9</title>
-<link href="css/styles.css" type="text/css" rel="stylesheet">
-</head>
-
-
-<body>
+    <head>
+        <link rel="stylesheet" href="css/styles.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" >
+        <title>Week 9</title>
+    </head>
+    <body>
 <header>
