8f5c2cc83ec860259ba4b8995cae00d4c4552a1e
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-12-16 06:31:58 -0800

    Update index.php

--- final-project/index.php
+++ final-project/index.php
@@ -16,10 +16,9 @@
         echo random_pics($photos);
     ?>
     </div>
-    <main>
     <h1><?php echo $headline;?></h1>
     <p>Mechanical keyboards are the keyboards that most people picture when they think about keyboards; they're the classic-looking, sturdy keyboards from the 1980s. A more proper definition is that mechanical keyboards are made with high-quality plastic key switches underneath each of the keycaps.</p>  
-    </main>
+    
 <?php 
 include('includes/footer.php');
 ?>
