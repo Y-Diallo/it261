d2236fec6a85bcbd193b0abb437368de565ba6ff
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-12-16 06:33:38 -0800

    about

--- final-project/about.php
+++ final-project/about.php
@@ -3,18 +3,11 @@
     include('includes/header.php');
 ?>
 <div id="wrapper">
-    <div id="hero">
-    <img id="image" src="images/streamerstable.png" alt="adminer table">
-    </div>
-    <main>
     <h1><?php echo $headline;?></h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </main>
+    <img id="image" src="images/streamerstable.png" alt="adminer table">
+    <img id="image" src="images/streamerstable.png" alt="adminer table">
+    
 
-    <aside>
-
-
-    </aside>
 <?php 
 include('includes/footer.php');
 ?>
--- final-project/index.php
+++ final-project/index.php
@@ -11,12 +11,13 @@
     );
 ?>
 <div id="wrapper">
+    <h1><?php echo $headline;?></h1>
     <div id="hero">
     <?php 
         echo random_pics($photos);
     ?>
     </div>
-    <h1><?php echo $headline;?></h1>
+    
     <p>Mechanical keyboards are the keyboards that most people picture when they think about keyboards; they're the classic-looking, sturdy keyboards from the 1980s. A more proper definition is that mechanical keyboards are made with high-quality plastic key switches underneath each of the keycaps.</p>  
     
 <?php 
