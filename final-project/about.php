4b6ee72292162c2a97703fd2c70a1b4e08600817
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2021-12-16 06:44:03 -0800

    home

--- final-project/about.php
+++ final-project/about.php
@@ -4,10 +4,8 @@
 ?>
 <div id="wrapper">
     <h1><?php echo $headline;?></h1>
-    <div class="content">
     <img id="image" src="images/streamerstable.png" alt="adminer table">
     <img id="image" src="images/streamerstable.png" alt="adminer table">
-    </div>
     
 
 <?php 
